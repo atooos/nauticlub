1197d75f23538c80539ef1fdcedef0a18174051f
Henri LEPIC <henrilepic@gmail.com>
2021-04-06 15:41:22 +0200

    add user entity

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/atooos/nauticlub
 
 go 1.16
 
-require github.com/gin-gonic/gin v1.6.3
+require (
+	github.com/gin-gonic/gin v1.6.3
+	github.com/google/uuid v1.2.0
+)
--- main.go
+++ main.go
@@ -1,13 +1,59 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"errors"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	r.GET("/users", GetUsers)
+	r.POST("/users", CreateUser)
+	//r.DELETE("/users", DeleteUser)
+	//r.PUT("/users", UpdateUser)
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
+
+var UserList = map[string]*User{}
+
+func GetUsers(ctx *gin.Context) {
+	ctx.JSON(200, UserList)
+}
+
+func CreateUser(ctx *gin.Context) {
+	var u User
+	err := ctx.BindJSON(&u)
+	if err != nil {
+		log.Println(err)
+		ctx.JSON(http.StatusBadRequest, nil)
+	}
+	err = u.ValidCreatePayload()
+	if err != nil {
+		log.Println(err)
+		ctx.JSON(http.StatusBadRequest, nil)
+	}
+	u.ID = uuid.NewString()
+	UserList[u.ID] = &u
+	ctx.JSON(200, u)
+}
+
+type User struct {
+	ID          string    `json:"uuid"`
+	FirstName   string    `json:"first_name"`
+	LastName    string    `json:"last_name"`
+	BirthDate   time.Time `json:"birth_date"`
+	Email       string    `json:"email"`
+	PhoneNumber string    `json:"phone_number"`
+}
+
+func (u *User) ValidCreatePayload() error {
+	if len(u.FirstName) == 0 {
+		return errors.New("empty first name")
+	}
+	return nil
 }
