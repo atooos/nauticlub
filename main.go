78a36ec2018a137d79cc79b9a43e019da83bcd5f
Henri LEPIC <henrilepic@gmail.com>
2021-04-06 15:57:36 +0200

    add DELETE and PUT endpoints

--- main.go
+++ main.go
@@ -14,8 +14,8 @@ func main() {
 	r := gin.Default()
 	r.GET("/users", GetUsers)
 	r.POST("/users", CreateUser)
-	//r.DELETE("/users", DeleteUser)
-	//r.PUT("/users", UpdateUser)
+	r.DELETE("/users/:uuid", DeleteUser)
+	r.PUT("/users/:uuid", UpdateUser)
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
 
@@ -31,15 +31,53 @@ func CreateUser(ctx *gin.Context) {
 	if err != nil {
 		log.Println(err)
 		ctx.JSON(http.StatusBadRequest, nil)
+		return
 	}
 	err = u.ValidCreatePayload()
 	if err != nil {
 		log.Println(err)
 		ctx.JSON(http.StatusBadRequest, nil)
+		return
 	}
 	u.ID = uuid.NewString()
 	UserList[u.ID] = &u
 	ctx.JSON(200, u)
+}
+
+func UpdateUser(ctx *gin.Context) {
+	uuid := ctx.Param("uuid")
+	_, ok := UserList[uuid]
+	if !ok {
+		ctx.JSON(http.StatusNotFound, nil)
+		return
+	}
+
+	var u User
+	err := ctx.BindJSON(&u)
+	if err != nil {
+		log.Println(err)
+		ctx.JSON(http.StatusBadRequest, nil)
+		return
+	}
+	err = u.ValidUpdatePayload()
+	if err != nil {
+		log.Println(err)
+		ctx.JSON(http.StatusBadRequest, nil)
+		return
+	}
+	UserList[uuid] = &u
+	ctx.JSON(http.StatusAccepted, u)
+}
+
+func DeleteUser(ctx *gin.Context) {
+	uuid := ctx.Param("uuid")
+	_, ok := UserList[uuid]
+	if !ok {
+		ctx.JSON(http.StatusNotFound, nil)
+		return
+	}
+	delete(UserList, uuid)
+	ctx.JSON(http.StatusAccepted, nil)
 }
 
 type User struct {
@@ -55,5 +93,9 @@ func (u *User) ValidCreatePayload() error {
 	if len(u.FirstName) == 0 {
 		return errors.New("empty first name")
 	}
+	return nil
+}
+
+func (u *User) ValidUpdatePayload() error {
 	return nil
 }
